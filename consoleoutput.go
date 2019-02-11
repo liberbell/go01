3a0841c6b8039f29832282e4baedb002a6f35146
liberbell <liberbell@gmail.com>
2019-02-12 07:41:30 +0900

    consoleoutput4

--- consoleoutput.go
+++ consoleoutput.go
@@ -21,4 +21,7 @@ func main() {
 	fmt.Printf("value of number: %v\n", aNumber)
 	fmt.Printf("Value of isTrue: %v\n", isTrue)
 	fmt.Printf("value of number as float: %.2f\n", float64(aNumber))
+
+	fmt.Printf("Data types: %T,%T,%T,%T, and,%T",
+		str1, str2, str3, aNumber, isTrue)
 }
