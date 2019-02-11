f2ebee21734d1f63eb8968daaecec2445c2992c7
liberbell <liberbell@gmail.com>
2019-02-12 07:36:14 +0900

    consoleoutput1

--- consoleoutput.go
+++ consoleoutput.go
@@ -8,14 +8,16 @@ func main() {
 	str1 := "The quick red fox"
 	str2 := "jumped over"
 	str3 := "the lazy brown dog."
-	// aNumber := 42
-	// isTrue := true
+	aNumber := 42
+	isTrue := true
 
-	stringLength, err := fmt.Println(str1, str2, str3)
+	// stringLength, err := fmt.Println(str1, str2, str3)
+	stringLength, _ := fmt.Println(str1, str2, str3)
 
-	if err == nil {
-		fmt.Println("String length:", stringLength)
-	}
+	// if err == nil {
+	fmt.Println("String length:", stringLength)
+	// }
 
-	fmt.Printf("value of number: %v")
+	fmt.Printf("value of number: %v\n", aNumber)
+	fmt.Printf("Value of isTrue: %v\n", isTrue)
 }
