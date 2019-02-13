0038bb472bf947a074f8723b27ce9e281e0b2042
liberbell <liberbell@gmail.com>
2019-02-14 07:47:12 +0900

    math4

--- math.go
+++ math.go
@@ -8,9 +8,9 @@ func main() {
 
 	i1, i2, i3 := 12, 45, 68
 	intsum := i1 + i2 + i3
-	fmt.Println(intsum)
+	fmt.Println("integer sum:", intsum)
 
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	fltsum := f1 + f2 + f3
-	fmt.Println(fltsum)
+	fmt.Println("float sum:", fltsum)
 }
