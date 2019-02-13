8924dc38a3c1011fa2803d0869164f209266ed52
liberbell <liberbell@gmail.com>
2019-02-14 07:53:53 +0900

    math5

--- math.go
+++ math.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+  "math/big"
 )
 
 func main() {
@@ -13,4 +14,9 @@ func main() {
 	f1, f2, f3 := 23.5, 65.1, 76.3
 	fltsum := f1 + f2 + f3
 	fmt.Println("float sum:  ", fltsum)
+
+  var b1, b2, b3 bigsum big.Float
+  b1.SetFloat64(23.5)
+  b2.SetFloat64(65.1)
+  b3.SetFloat64(76.3)
 }
