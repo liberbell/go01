012161ec9cc54d3fe424296f1b341b230f8f466d
liberbell <liberbell@gmail.com>
2019-02-15 08:42:05 +0900

    datesandtimers4

--- datesandtimers.go
+++ datesandtimers.go
@@ -8,4 +8,7 @@ import (
 func main() {
 	t := time.Date(2019, time.February, 15, 8, 37, 0, 0, time.UTC)
 	fmt.Printf("Go launched at %s\n", t)
+
+	now := time.Now()
+	fmt.Printf("The time now is %s\n", now)
 }
