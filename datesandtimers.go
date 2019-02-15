10e9d164dd0687cbd04934a828177289c4673766
liberbell <liberbell@gmail.com>
2019-02-15 09:03:01 +0900

    datesandtimers9

--- datesandtimers.go
+++ datesandtimers.go
@@ -22,6 +22,8 @@ func main() {
 
 	longFormat := "Friday, February 15, 2019"
 	fmt.Println("Tomorrow is ", tomorrow.Format(longFormat))
+
 	shortFormat := "2/15/19"
 	fmt.Println("Tomorrow is ", tomorrow.Format(shortFormat))
+
 }
