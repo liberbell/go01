e47fbfbeeb7110e8c6e6ff188154cbc2a44dc7e4
liberbell <liberbell@gmail.com>
2019-02-19 07:53:45 +0900

    structs6

--- structs.go
+++ structs.go
@@ -11,5 +11,5 @@ func main() {
 	poodle := Dog{"Poodle", 20}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
-	fmt.Printf("Breed: %v\nWeight: %v", poodle.Breed, poodle.Weight)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
 }
