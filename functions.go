5acd9c0b68149ebc817c6c2638ef300ca0786bdd
liberbell <liberbell@gmail.com>
2019-02-22 21:31:01 +0900

    functions6

--- functions.go
+++ functions.go
@@ -6,6 +6,8 @@ func main() {
 	dosomething()
 	sum := AddValues(23, 54)
 	fmt.Println("Sum: ", sum)
+
+  sum AddAllvalues(10, 23, 44)
 }
 
 func dosomething() {
@@ -14,4 +16,10 @@ func dosomething() {
 
 func AddValues(value1 int, value2 int) int {
 	return value1 + value2
+}
+
+fun AddAllValues(values ...int) int {
+  sum := 0
+  fmt.Printf("%T\n", values)
+  return sum
 }
