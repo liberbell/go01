4c8cac0e712f9c52333a40b5c52ac75b01cf213b
liberbell <liberbell@gmail.com>
2019-02-23 14:41:25 +0900

    multiReturn7

--- multiReturn.go
+++ multiReturn.go
@@ -6,7 +6,7 @@ func main() {
 	n1, l1 := FullName("Zaphad", "Beeblebrox")
 	fmt.Printf("Fullname: %v, Number of chars: %v\n", n1, l1)
 
-	n2, l2 := FullNameNakedReturn("Zaphad", "Beeblebrox")
+	n2, l2 := FullNameNakedReturn("Arthur", "Dent")
 	fmt.Printf("Fullname: %v, Number of chars: %v\n", n2, l2)
 }
 
