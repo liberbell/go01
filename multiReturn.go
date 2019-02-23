52c3dc544227b3dfae33603bd52732f7e26098df
liberbell <liberbell@gmail.com>
2019-02-23 14:40:40 +0900

    multiReturn5

--- multiReturn.go
+++ multiReturn.go
@@ -5,6 +5,9 @@ import "fmt"
 func main() {
 	n1, l1 := FullName("Zaphad", "Beeblebrox")
 	fmt.Printf("Fullname: %v, Number of chars: %v\n", n1, l1)
+
+	n2, l2 := FullNameNakedReturn("Zaphad", "Beeblebrox")
+	fmt.Printf("Fullname: %v, Number of chars: %v\n", n2, l2)
 }
 
 func FullName(f, l string) (string, int) {
