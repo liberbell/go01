b7940b1decc92c59d0dac89441ba5aa8ddedd538
liberbell <liberbell@gmail.com>
2019-02-25 07:58:49 +0900

    errors1

--- errors.go
+++ errors.go
@@ -1,7 +1,15 @@
 package main
 
-import "os"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 	f, err := os.Open("Filename.ext")
+	if err == nil {
+		fmt.Println(f)
+	} else {
+		fmt.Println(err)
+	}
 }
