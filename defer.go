e4fcae5de627793d4cf04d01240671b641937146
liberbell <liberbell@gmail.com>
2019-02-26 07:41:03 +0900

    defer4

--- defer.go
+++ defer.go
@@ -12,3 +12,8 @@ func main() {
 	defer fmt.Println("Statement4")
 	fmt.Println("Undefered Statement")
 }
+
+func myFunc() {
+	defer fmt.Println("Deferred in the function")
+	fmt.Println("Not Deferred in the function")
+}
