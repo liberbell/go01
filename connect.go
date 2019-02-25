b4c98ea3491242264dccce994592959e8d536e90
liberbell <liberbell@gmail.com>
2019-02-26 07:50:34 +0900

    connect3

--- connect.go
+++ connect.go
@@ -9,3 +9,11 @@ func main() {
 	doSomething()
 	fmt.Println("Connection open: %v\n", isConnected)
 }
+
+func doSomething() {
+	connect()
+	fmt.Println("Deffering disconnect!")
+	defer disconnect()
+	fmt.Println("Connection open: %v\n", isConnected)
+	fmt.Println("Doing something!")
+}
