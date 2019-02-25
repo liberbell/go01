0571b022e2cec1a697b34c0dd161d9ff68c2357a
liberbell <liberbell@gmail.com>
2019-02-26 07:54:19 +0900

    connect6

--- connect.go
+++ connect.go
@@ -5,16 +5,16 @@ import "fmt"
 var isConnected bool = false
 
 func main() {
-	fmt.Println("Connection open: %v\n", isConnected)
+	fmt.Printf("Connection open: %v", isConnected)
 	doSomething()
-	fmt.Println("Connection open: %v\n", isConnected)
+	fmt.Printf("Connection open: %v\n", isConnected)
 }
 
 func doSomething() {
 	connect()
 	fmt.Println("Deffering disconnect!")
 	defer disconnect()
-	fmt.Println("Connection open: %v\n", isConnected)
+	fmt.Printf("Connection open: %v\n", isConnected)
 	fmt.Println("Doing something!")
 }
 
