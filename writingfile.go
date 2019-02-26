e333d83c1f8820cacd9e3dfebbc7cc2ef55944a2
liberbell <liberbell@gmail.com>
2019-02-27 07:53:36 +0900

    writingfile6

--- writingfile.go
+++ writingfile.go
@@ -16,7 +16,7 @@ func main() {
 	ln, err := io.WriteString(file, content)
 	checkError(err)
 
-	fmt.Printf("All done with file of %v characters", ln)
+	fmt.Printf("All done with file of %v characters\n", ln)
 }
 
 func checkError(err error) {
