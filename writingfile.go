c6b37916f51de2426c1a5d6884c75da2d0b4df0f
liberbell <liberbell@gmail.com>
2019-02-27 07:51:37 +0900

    writingfile4

--- writingfile.go
+++ writingfile.go
@@ -1,12 +1,22 @@
 package main
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
 func main() {
 	content := "Hello from Go!"
 
 	file, err := os.Create("./fromstring.txt")
+	checkError(err)
+	defer file.Close()
+
+	ln, err := io.WriteString(file, content)
 }
 
 func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
