c99e2626cdb1dcfa362f8760f2b0439dd4910281
liberbell <liberbell@gmail.com>
2019-03-01 08:36:52 +0900

    parseJson3

--- parseJson.go
+++ parseJson.go
@@ -1,1 +1,16 @@
 package main
+
+import "fmt"
+
+func main() {
+	url := "http://services.explorecalifornia.org/json/tours.php"
+	content := contentFromServer(url)
+
+	fmt.Println(content)
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
