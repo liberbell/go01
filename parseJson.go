884ad38931812318f07cff6e8ca3753b18530b52
liberbell <liberbell@gmail.com>
2019-03-01 08:40:06 +0900

    parseJson4

--- parseJson.go
+++ parseJson.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 	url := "http://services.explorecalifornia.org/json/tours.php"
@@ -13,4 +17,15 @@ func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func contentFromServer(url string) string {
+	resp, err := http.Get(url)
+	checkError(err)
+
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+
+	return string(bytes)
 }
