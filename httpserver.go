5da668b857fc30f29efbfd50fcf5a097af54e54d
liberbell <liberbell@gmail.com>
2019-03-02 22:44:23 +0900

    httpserver3

--- httpserver.go
+++ httpserver.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+type hello struct{}
+
+func (h hello) serveHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>Hello from the Go web server!</h1>")
+}
+
 func main() {
 }
 
